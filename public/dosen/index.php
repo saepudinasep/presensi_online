869cfce21a69d068a3ac76b0583f3954f859c991
saepudinasep2001@gmail.com <saepudinasep2001@gmail.com>
2022-08-08 12:32:34 +0700

    comment

---
+++ public/dosen/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard Dosen</title>
+    <!-- link bootstrap -->
+    <link rel="stylesheet" href="../../dist/css/style.css">
+    <link rel="stylesheet" href="../../dist/css/bootstrap.min.css">
+  </head>
+  <body>
+      <!-- start header -->
+      <!-- <div class="header">
+
+      </div> -->
+      <!-- end header -->
+      <!-- link javascript -->
+      <script src="../../dist/js/bootstrap.bundle.min.js"></script>
+      <!-- end link javascript -->
+  </body>
+</html>
